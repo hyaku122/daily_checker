9025a961527f847d87ac4fdd4633ebc47f88d740
hyaku122 <ogatahyakunosuke0122@gmail.com>
2026-05-03 20:23:18 +0900

    ７

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function () {
-  var APP_VERSION = "2026.05.03-16";
+  var APP_VERSION = "2026.05.03-17";
   var CACHE_PREFIX = "mino-kumoyou-static-";
   var BACKUP_PREFIX = "mino-kumoyou-backup-v1:";
   var STORAGE_KEYS = {
